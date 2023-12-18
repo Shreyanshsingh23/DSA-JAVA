991cd0c354e74394c95cf465fee56c05893fe9d3
Shreyanshsingh23 <shreyanshrewa@gmail.com>
2023-12-19 01:31:50 +0530

    Update leetcode_1295_evendigit.java

--- leetcode_1295_evendigit.java
+++ leetcode_1295_evendigit.java
@@ -15,8 +15,16 @@ public class leetcode_1295_evendigit {
         }
     }
     return ans;
+
+
  }
  
+ // Alternate method is to convert the digit into string and then take the length of the string 
+ // create a fumction and then call the function foe every element of rhe array using loop
+ //function will check that whether number contains even digits or not
+ // return type will be boolean
+ // if length of string is even then counnt++
+ // as simple as that
    
     
 }
