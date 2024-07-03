081c8a48375a8033490b7d26121bf8c54213d3e1
Shreyanshsingh23 <shreyanshrewa@gmail.com>
2024-07-03 19:38:30 +0530

    .

--- quicksort.java
+++ quicksort.java
@@ -1,39 +1,46 @@
+import java.util.Arrays;
+
 public class quicksort {
     public static void main(String[] args) {
         int[] arr = {1,3,2,4,5};
         sort(arr,0,arr.length-1);
-        System.out.println(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    static void sort(int[] arr, int s, int e){
+    static void sort(int[] arr, int low, int high){
        // base condition
-        if(e-s == 0){
+        if(low<=high){
         return;
        }
-
+       int s = low;
+       int e = high;
        int pivot = s + (e-s)/2;
 
         while(s<=e){
             
-            // selecting s element which is greater than pivot so can swap it
+            // selecting 's' element which is greater than pivot so can swap it
             while(arr[s] < arr[pivot]){
                 s++;
             }
 
-             // selecting e element which is greater than pivot so can swap it
+             // selecting 'e' element which is greater than pivot so can swap it
              while(arr[e] > arr[pivot]){
                 e--;
             }
 
+            // this check is the reason why it will not swap if its already sorted, unlike merge sort
+            if(s<=e){
             swap(arr,s,e);
             s++;
-            e--;
+            e--;}
 
 
         }
 
-        sort(arr,0,e);
-        sort(arr,s,arr.length-1);
+        //now my array is sorted, I need to sort two halves now
+
+        sort(arr,low,e);
+        sort(arr,s,high);
     }
 
     static void swap(int[] arr, int f , int s){
